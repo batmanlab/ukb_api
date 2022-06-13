7e8f965c1123c897e83cb4d6e7e1dfa640e9911c
Tigmanshu Chaudhary <tic48@pitt.edu>
2022-06-12 21:55:18 -0400

    sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,12 +12,13 @@
 #
 import os
 import sys
+sys.path.insert(0, os.path.abspath('../../ukb_api'))
 # sys.path.insert(0, '/media/tighu/extended_storage/ukb_api/ukb_api')
 # sys.path.insert(0, '/media/tighu/extended_storage/ukb_api/ukb_api')
 # sys.path.insert(0, os.path.abspath('../..'))
 # sys.path.insert(0, os.path.abspath('../../ukb_api/module_bulk_data_handler'))
-sys.path.insert(0, os.path.abspath('../../ukb_api/'))
-sys.path.insert(0, os.path.abspath('../../ukb_api/module_bulk_data_handler.py'))
+# sys.path.insert(0, os.path.abspath('../../ukb_api/'))
+# sys.path.insert(0, os.path.abspath('../../ukb_api/module_bulk_data_handler.py'))
 # for x in os.walk('../../ukb_api'):
 #   sys.path.insert(0, x[0])
 
