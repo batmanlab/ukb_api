ea0093d5616d5380887c307d86173ea1269c1653
tighu20 <tic48@pitt.edu>
2022-07-08 10:13:36 -0400

    sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(1, os.path.abspath('../../'))
+sys.path.insert(1, os.path.abspath('../../ukb_api/'))
 # sys.path.insert(0, os.path.abspath('../../ukb_api'))
 # sys.path.insert(0, os.path.abspath('../../ukb_api/module_bulk_data_handler'))
 # sys.path.insert(0, os.path.abspath('../../ukb_api/module_scalar_data_handler'))
